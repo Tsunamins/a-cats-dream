f93347d25a91c728ee2a068af45f531add270170
Tsunamins <reillyamr@gmail.com>
2019-12-26 13:16:00 -0500

    add games table

--- dream-back/db/migrate/20191226181019_create_games.rb
+++ dream-back/db/migrate/20191226181019_create_games.rb
@@ -1,7 +1,8 @@
 class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
-
+      t.string :game_save
+      t.belongs_to :user, foreign_key: true
       t.timestamps
     end
   end
--- dream-back/db/schema.rb
+++ dream-back/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_26_172936) do
+ActiveRecord::Schema.define(version: 2019_12_26_181019) do
+
+  create_table "games", force: :cascade do |t|
+    t.string "game_save"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_games_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
