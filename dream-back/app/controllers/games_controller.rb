aa4973abcd2853388812aa97364e0bd578ceb501
Tsunamins <reillyamr@gmail.com>
2019-12-26 13:20:08 -0500

    add to games controller

--- dream-back/app/controllers/games_controller.rb
+++ dream-back/app/controllers/games_controller.rb
@@ -5,5 +5,34 @@ class GamesController < ApplicationController
         render json: @games, status: 200 
     end
 
-    
+    def show
+        @game = Game.find(params[:id])
+        render json: @game, status: 200
+    end
+
+    def create
+       
+        @game = Game.create(game_params)
+        render json: @game, status: 200
+    end
+
+    def update
+       
+        @game = Game.find(params[:id])
+        @game.update(game_params)
+        render json: @game, status: 200
+    end
+
+    def destroy
+        @game = Game.find(params[:id])
+        @game.delete
+        render json: {gameId: @game.id} 
+    end
+
+    private
+        def game_params
+            params.require(:game).permit(:game_save, :user_id) #for now
+        end
+
+
 end
