453d6eea8f411a6ab9c321b50f171a7d15a49383
Tsunamins <reillyamr@gmail.com>
2019-12-26 13:35:41 -0500

    add CRUD to stats controller

--- dream-back/app/controllers/stats_controller.rb
+++ dream-back/app/controllers/stats_controller.rb
@@ -1,2 +1,35 @@
 class StatsController < ApplicationController
+    def index
+        @stats = Stat.all
+        render json: @stats, status: 200 
+    end
+
+    def show
+        @stat = Stat.find(params[:id])
+        render json: @stat, status: 200
+    end
+
+    def create
+       
+        @stat = Stat.create(stat_params)
+        render json: @stat, status: 200
+    end
+
+    def update
+       
+        @stat = Stat.find(params[:id])
+        @stat.update(stat_params)
+        render json: @stat, status: 200
+    end
+
+    def destroy
+        @stat = Stat.find(params[:id])
+        @stat.delete
+        render json: {statId: @stat.id} 
+    end
+
+    private
+        def stat_params
+            params.require(:stat).permit(:fireflies_collected, :enemies_defeated, :hit_by_enemy, :strategy_score, :game_id) #for now
+        end
 end
