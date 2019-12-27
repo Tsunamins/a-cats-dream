e1095f3e046ca20c3980c32d685dbef11cf15737
Tsunamins <reillyamr@gmail.com>
2019-12-27 14:32:35 -0500

    add player from position in json data file

--- dream-front/src/game/GameScene.js
+++ dream-front/src/game/GameScene.js
@@ -1,10 +1,12 @@
+//import Player from 'Player.js';
+
 class GameScene extends Phaser.Scene {
     constructor(){
         super({
             key: 'GameScene'
         })
 
-        this.map
+      this.player;  
     }
 
     
@@ -12,18 +14,25 @@ class GameScene extends Phaser.Scene {
     preload(){ 
         this.load.image('tiles', './assets/maps/pinktilesheet.png');
         this.load.tilemapTiledJSON('map', './assets/maps/1stworkingmap.json');
+        this.load.spritesheet('player', './assets/images/catspritesheet.png', { frameWidth: 35, frameHeight: 32 });
     }
 
     create(){
         //add map
         this.map = this.make.tilemap({key: 'map'});
-       
         this.tiles = this.map.addTilesetImage('pinktilesheet', 'tiles');
-
         this.layer_background = this.map.createStaticLayer('background', this.tiles, 0, 0);
         this.layer_roads = this.map.createStaticLayer('roads', this.tiles, 0, 0);
         this.layer_collision = this.map.createStaticLayer('buildings-trees', this.tiles, 0, 0);
         this.layer_collision.setCollisionByExclusion([-1], true, this);
+
+        //add player from Tiled definitions
+        this.map.findObject('objects', (obj) => {
+            if (obj.type === 'player'){
+            this.player = this.physics.add.sprite(obj.x, obj.y, 'player')
+            }
+            //if this ends up in a separate player/class file need new Player(,,)
+        });
 
     }
 
