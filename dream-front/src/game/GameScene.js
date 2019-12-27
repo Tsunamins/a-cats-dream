1c47409a995b723d5cc6138073ac081665c6573c
Tsunamins <reillyamr@gmail.com>
2019-12-27 14:43:33 -0500

    add player world collision and animations

--- dream-front/src/game/GameScene.js
+++ dream-front/src/game/GameScene.js
@@ -18,7 +18,7 @@ class GameScene extends Phaser.Scene {
     }
 
     create(){
-        //add map
+        //create map
         this.map = this.make.tilemap({key: 'map'});
         this.tiles = this.map.addTilesetImage('pinktilesheet', 'tiles');
         this.layer_background = this.map.createStaticLayer('background', this.tiles, 0, 0);
@@ -26,7 +26,7 @@ class GameScene extends Phaser.Scene {
         this.layer_collision = this.map.createStaticLayer('buildings-trees', this.tiles, 0, 0);
         this.layer_collision.setCollisionByExclusion([-1], true, this);
 
-        //add player from Tiled definitions
+        //create player from Tiled definitions
         this.map.findObject('objects', (obj) => {
             if (obj.type === 'player'){
             this.player = this.physics.add.sprite(obj.x, obj.y, 'player')
@@ -34,6 +34,43 @@ class GameScene extends Phaser.Scene {
             //if this ends up in a separate player/class file need new Player(,,)
         });
 
+        //create player specifics, listed first will prob go in Player class, second uncertain or need to stay out
+
+            //create world collision
+            player.setCollideWorldBounds(true);
+
+            //create player animations
+            this.anims.create({
+                //changed from left to walking to apply flipX instead
+                key: 'walking',
+                frames: this.anims.generateFrameNumbers('player', { start: 0, end: 2 }),
+                frameRate: 10,
+                repeat: -1
+              });
+          
+              this.anims.create({
+                  key: 'turn',
+                  frames: [ { key: 'player', frame: 3 } ],
+                  frameRate: 20
+              });
+              
+              //firing key not working, frame error
+              // this.anims.create({
+              //   key: 'fire',
+              //   frames: [ { key: 'player', frame: 4 } ],
+              //   frames: 20
+              // });
+    }
+
+    update(){
+         //create player movement based on touch/mouse events, want to keep for mobile possibilities
+         if(this.input.activePointer.isDown){
+            player.x += (pointer.x - player.x) * 0.05;
+            player.y += (pointer.y - player.y) * 0.05;
+            
+            };
+
+            
     }
 
 
