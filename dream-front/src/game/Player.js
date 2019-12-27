aa031064864d2906addf3ea336664f5dd97a1628
Tsunamins <reillyamr@gmail.com>
2019-12-27 14:33:01 -0500

    add Player class for possible usage instead

---
+++ dream-front/src/game/Player.js
@@ -0,0 +1,13 @@
+export default class Player extends Phaser.Physics.Arcade.Sprite{
+    constructor(scene, x, y){
+        super(scene, x, y, 'characters', 325);
+        this.scene = scene;
+
+        this.scene.physics.world.enable(this);
+
+        this.scene.add.existing(this);
+        
+    }
+
+    
+}
