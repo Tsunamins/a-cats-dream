262dcbbc853bf6091b9f6015f572270476b86335
Tsunamins <reillyamr@gmail.com>
2019-12-28 13:28:51 -0500

    change to Player class, and add new Player

--- dream-front/src/game/GameScene.js
+++ dream-front/src/game/GameScene.js
@@ -1,4 +1,4 @@
-//import Player from 'Player.js';
+import Player from './Player.js';
 import Magic from './Magic.js';
 
 class GameScene extends Phaser.Scene {
@@ -36,10 +36,47 @@ class GameScene extends Phaser.Scene {
         //create player from Tiled definitions
         this.map.findObject('objects', (obj) => {
             if (obj.type === 'player'){
-            this.player = this.physics.add.sprite(obj.x, obj.y, 'player')
+                this.player = new Player(this, obj.x, obj.y)
+            //this.player = this.physics.add.sprite(obj.x, obj.y, 'player')
             }
             //if this ends up in a separate player/class file need new Player(,,)
         });
+
+        //player animations
+        this.anims.create({
+            //changed from left to walking to apply flipX instead
+            key: 'walking',
+            frames: this.anims.generateFrameNumbers('player', { start: 0, end: 2 }),
+            frameRate: 10,
+            repeat: -1
+          });
+      
+          this.anims.create({
+              key: 'turn',
+              frames: [ { key: 'player', frame: 3 } ],
+              frameRate: 20
+          });
+          
+          //firing key not working, frame error
+          // this.anims.create({
+          //   key: 'fire',
+          //   frames: [ { key: 'player', frame: 4 } ],
+          //   frames: 20
+          // });
+
+          //create world collision, cat currently stopping at 600, will change all dimensions later with new map and config
+        //   this.physics.add.existing(this.player);
+        //   this.player.setCollideWorldBounds(true);
+
+    //create cameras for tracking player movement
+    //will want to adjust this, or maybe works well enough with line startFollow
+    //this.cameras.main.setSize(400, 300);
+    this.cameras.main.startFollow(this.player);
+
+
+
+
+    /*******magic */
 
          /****working with original class */
        this.magics = this.physics.add.group({ classType: Magic, runChildUpdate: true });
@@ -53,81 +90,18 @@ class GameScene extends Phaser.Scene {
         //create input
         this.pointer = this.input.activePointer;
         this.cursors = this.input.keyboard.createCursorKeys();
-
-        //create player specifics, listed first will prob go in Player class, second uncertain or need to stay out
-
-            //create world collision, cat currently stopping at 600, will change all dimensions later with new map and config
-            this.physics.add.existing(this.player);
-            this.player.setCollideWorldBounds(true);
-
-            //create player animations
-            this.anims.create({
-                //changed from left to walking to apply flipX instead
-                key: 'walking',
-                frames: this.anims.generateFrameNumbers('player', { start: 0, end: 2 }),
-                frameRate: 10,
-                repeat: -1
-              });
-          
-              this.anims.create({
-                  key: 'turn',
-                  frames: [ { key: 'player', frame: 3 } ],
-                  frameRate: 20
-              });
-              
-              //firing key not working, frame error
-              // this.anims.create({
-              //   key: 'fire',
-              //   frames: [ { key: 'player', frame: 4 } ],
-              //   frames: 20
-              // });
-
-        //create cameras for tracking player movement
-        //will want to adjust this, or maybe works well enough with line startFollow
-        //this.cameras.main.setSize(400, 300);
-        this.cameras.main.startFollow(this.player);
-    }
+        }
 
     update(){
-        //create player movement based on touch/mouse events, want to keep for mobile possibilities
+        //keyboard movement called from Player class
+        this.player.update(this.cursors);
+
+        //mouse/touch event added in case mobile
         if(this.input.activePointer.isDown){
             this.player.x += (this.pointer.x - this.player.x) * 0.05;
-            this.player.y += (this.pointer.y - this.player.y) * 0.05;
-            
-            };
-
-            
-
-            if(this.cursors.left.isDown){
-                this.player.flipX = true;
-                this.player.setVelocityX(-160);
-                this.player.anims.play('walking', true);
+            this.player.y += (this.pointer.y - this.player.y) * 0.05;   
+        };
         
-            } else if (this.cursors.right.isDown){
-                this.player.flipX = false;
-                this.player.setVelocityX(160);
-                this.player.anims.play('walking', true);
-
-            } else { 
-                this.player.setVelocityX(0);
-                this.player.anims.play('turn', true);
-    
-            };
-    
-            if(this.cursors.up.isDown){
-                this.player.setVelocityY(-160);
-                this.player.anims.play('walking', true);
-      
-             
-          } else if (this.cursors.down.isDown){
-                this.player.setVelocityY(160);
-                this.player.anims.play('walking', true);
-      
-            
-          } else {   
-            this.player.setVelocityY(0); 
-          };
-
           //**************************************** */
           //firing section
           if (this.cursors.space.isDown){
--- dream-front/src/game/Player.js
+++ dream-front/src/game/Player.js
@@ -1,13 +1,55 @@
 export default class Player extends Phaser.Physics.Arcade.Sprite{
     constructor(scene, x, y){
-        super(scene, x, y, 'characters', 325);
+        super(scene, x, y, 'player');
         this.scene = scene;
-
+       
         this.scene.physics.world.enable(this);
 
         this.scene.add.existing(this);
         
+        this.setCollideWorldBounds(true);
+
+        //these may or may not be needed
+        //this.physics.add.existing(this.player);
+       // scene.add.existing(this);
+        // scene.physics.add.existing(this);
+
+        
     }
+
+    update(cursors){
+        if(cursors.left.isDown){
+            this.flipX = true;
+            this.setVelocityX(-160);
+            this.anims.play('walking', true);
+    
+        } else if (cursors.right.isDown){
+            this.flipX = false;
+            this.setVelocityX(160);
+            this.anims.play('walking', true);
+
+        } else { 
+            this.setVelocityX(0);
+            this.anims.play('turn', true);
+
+        };
+
+        if(cursors.up.isDown){
+            this.setVelocityY(-160);
+            this.anims.play('walking', true);
+  
+         
+      } else if (cursors.down.isDown){
+            this.setVelocityY(160);
+            this.anims.play('walking', true);
+  
+        
+      } else {   
+        this.setVelocityY(0); 
+      };
+
+    }
+
 
     
 }
