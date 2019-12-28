443f1c79ab2ac5413cece2942642dc620f6b142a
Tsunamins <reillyamr@gmail.com>
2019-12-28 14:42:38 -0500

    add enemy class

---
+++ dream-front/src/game/Enemy.js
@@ -0,0 +1,37 @@
+export default class Enemy extends Phaser.Physics.Arcade.Sprite{
+    constructor(scene, x, y){
+        super(scene, x, y, 'enemy');
+        this.scene = scene;
+
+ 
+        // this.children.iterate(function(enemy){
+        //     this.x = Phaser.Math.Between(100, 600);
+        //      this.y = Phaser.Math.Between(100, 300)
+        //      this.setCollideWorldBounds(true);
+        //      this.setBounce(1);
+        //     this.setVelocity(Phaser.Math.Between(20, 60), Phaser.Math.Between(20, 60));
+           
+        //      this.health = 3;
+        // })
+     
+
+        // this.enemies.children.iterate(function (enemy){
+        
+        //     enemy.x = Phaser.Math.Between(100, 600);
+        //     enemy.y = Phaser.Math.Between(100, 300)
+        //     enemy.setCollideWorldBounds(true);
+        //     enemy.setBounce(1);
+        //     enemy.setVelocity(Phaser.Math.Between(20, 60), Phaser.Math.Between(20, 60));
+           
+        //     enemy.health = 3;
+        // });
+
+
+
+    }
+
+    //create(){ 
+        
+
+   // }
+}
