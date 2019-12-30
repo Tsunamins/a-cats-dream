b0adf28a108a625dd2c9f2e8f7d1c5da853d3260
Tsunamins <reillyamr@gmail.com>
2019-12-30 14:45:42 -0500

    add start game text

--- dream-front/src/game/TitleScene.js
+++ dream-front/src/game/TitleScene.js
@@ -3,16 +3,25 @@ class TitleScene extends Phaser.Scene {
         super({
             key: 'TitleScene'
         })
+       
+        
+
     }
 
+  
     preload(){ 
         this.load.image('background', './assets/images/pinkscenelogo800600.png');
+        
     }
 
     create(){
        let background = this.add.image(0, 0, 'background');//dimensions for now at least
        background.setOrigin(0,0);
 
+     
+        this.startGameText = this.add.text(200, 300, 'Welcome, Click to Play', { fontSize: '20px', fill: '#000',  backgroundColor: '#cebff5', fontFamily: 'Lucida Console'});
+
+     
        this.input.once('pointerdown', function () {
         
         this.scene.start('GameScene');
@@ -22,6 +31,7 @@ class TitleScene extends Phaser.Scene {
 
     update(){
         //console.log(this.sys.canvas.width)
+     
         
 
     }
