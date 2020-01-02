fa7aae2e7ca0c504f80894ea4f7e07535a9eddd2
Tsunamins <reillyamr@gmail.com>
2020-01-02 14:46:11 -0500

    remove excess notes

--- dream-back/app/controllers/users_controller.rb
+++ dream-back/app/controllers/users_controller.rb
@@ -12,20 +12,13 @@ class UsersController < ApplicationController
 
     def create 
         @user = User.find_or_create_by(email: user_params[:email])
-      
-    #   if @user.valid?
-    #     @user.save
-        #if using sessions
-        #session[:user_id] = @user.id
-        
-      #else
+
       render json: @user, status: 200
       
     end
 
     def update
-        #if using sessions and passwords, this below or something similar
-        #@user = current_user   
+       
         @user = User.find(params[:id])
         @user.update(user_params)
         render json: @user, status: 200
@@ -40,7 +33,7 @@ class UsersController < ApplicationController
 
     private
         def user_params
-            params.require(:user).permit(:email) #for now
+            params.require(:user).permit(:id, :email) #for now
         end 
 
     
