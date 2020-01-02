13f0171d1cdd0af3fbc5bec29d63540af8085566
Tsunamins <reillyamr@gmail.com>
2020-01-02 11:36:45 -0500

    restructure Users class

--- dream-front/src/ui/users.js
+++ dream-front/src/ui/users.js
@@ -1,13 +1,20 @@
 class Users {
     constructor(currentUser) {
-        this.currentUser = currentUser;  
-        this.userEvents();       
+        this.currentUser = currentUser;
+        this.userElements();
+        this.createUser();       
     }
-    userEvents(){
 
-        //get the div that will display the current user
+    userElements(){
+        //find or create user elements
         this.setCurrentUser = document.getElementById('current-user')
         this.userEmailInput = document.getElementById('user-email')
+
+        //update user elements and event
+        this.updateUser = document.getElementById('update-form')
+        this.updateEmail = document.getElementById('user-update')
+    }
+    createUser(){     
         UsersAdapter.createUser(this.currentUser).then(user => { 
             //setting this here will need to create a "logout button with localStorage.clear()"
         //    localStorage.setItem('user_id', user.id)
@@ -15,14 +22,12 @@ class Users {
           
             this.render(this.currentUser) 
             this.userEmailInput.value = ""
-        })
+        })       
     }
-
-
     render(value){    
         const h3 = document.createElement('h3');
         const userFunctions = document.getElementById('user-functions')
-        const removeForm = document.getElementById("user-info")
+        const removeForm = document.getElementById("user-info-form")
         //adds an update form but doesn't work well with eventlisteners since it doesn't exist yet
         h3.innerHTML = `
             <h3 id="user">${value}</h3>`;
