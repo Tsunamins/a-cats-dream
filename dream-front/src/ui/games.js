eacedcd3ce16eeaab3bb4636c39397cc4b74b41f
Tsunamins <reillyamr@gmail.com>
2020-01-04 22:53:02 -0500

    move game display items to user display items

--- dream-front/src/ui/games.js
+++ dream-front/src/ui/games.js
@@ -1,19 +1,31 @@
 class Games {
     constructor(){
-        this.gameUI();
+         //document.addEventListener('DOMContentLoaded', (event) => {
+             //console.log('DOM fully loaded and parsed');
+            this.gameUI();
+        //});
+       
         this.arrayFF = [];
         this.arrayEn = [];
         this.arrayAllStats = [];
+        
+  
 
     }
-
+  
     gameUI(){
         const gameData = document.getElementById('game-functions')
         const gameList = document.getElementById('game-list')
-        let id = localStorage.getItem('user_id')
-        UsersAdapter.getUserStats(id).then(stats => {
+        let user_id = localStorage.getItem('user_id')
+        console.log(localStorage.getItem('user_id'))
+        console.log(user_id)
+        UsersAdapter.getUserStats(user_id).then(stats => {
+            console.log(stats)
+         
             stats.forEach(stat => 
             this.arrayAllStats.push(stat))
+            
+        
             this.arrayAllStats.slice(1).slice(-5).forEach(stat =>{
                 this.arrayFF.push(stat.fireflies_collected)
                 this.arrayEn.push(stat.enemies_defeated)
@@ -21,10 +33,12 @@ class Games {
                 li.innerText = `Fireflies collected: ${stat.fireflies_collected}` + ` Enemies Banished: ${stat.enemies_defeated}`
                 gameList.appendChild(li)
             })
+        
         const fireFlyH3 = document.createElement('h3')
         const enemyH3 = document.createElement('h3')
-        let addFF = this.arrayFF.reduce(add)
-        let addEn = this.arrayEn.reduce(add)
+        let addFF = this.arrayFF.reduce((total, add) => total + add, 0);
+        
+        let addEn = this.arrayEn.reduce((total, add) => total + add, 0);
         fireFlyH3.innerText = `All Time Fireflies Collected: ${addFF}`
         enemyH3.innerText = `All Time Enemies Banished: ${addEn}`
         gameData.appendChild(fireFlyH3)
@@ -33,20 +47,14 @@ class Games {
         console.log(this.arrayAllStats)
                              
         
-
+      
         })
-
-        function add(total, index){
-            return total += index
-        }
-        
-        
-  
-
+    
+      
     }
 
    
-
+    
 
 
 
--- dream-front/src/ui/users.js
+++ dream-front/src/ui/users.js
@@ -2,13 +2,22 @@ class Users {
     constructor(currentUser) {
         this.currentUser = currentUser;
         this.userElements();
-        this.createUser();       
+        this.createUser();  
+        this.users = [];
+        //this.gameUI();
+        this.arrayFF = [];
+        this.arrayEn = [];
+        this.arrayAllStats = [];
+        
     }
 
     userElements(){
         //find or create user elements
         this.setCurrentUser = document.getElementById('current-user')
         //this.userEmailInput = document.getElementById('user-email')
+
+        this.gameData = document.getElementById('game-functions')
+        this.gameList = document.getElementById('game-list')
 
         //update user elements and event
         this.updateUser = document.getElementById('update-form')
@@ -18,14 +27,58 @@ class Users {
     createUser(){     
         UsersAdapter.createUser(this.currentUser).then(user => { 
             //setting this here will need to create a "logout button with localStorage.clear()"
-        //    localStorage.setItem('user_id', user.id)
+            localStorage.setItem('user_id', user.id)
         //    localStorage.setItem('user_email', user.email)
+        this.users.push(new User(user))
         console.log(user)
           
             this.render(this.currentUser) 
             this.userEmailField.value = ""
+            this.gameUI();
         })       
     }
+
+    gameUI(){
+        // const gameData = document.getElementById('game-functions')
+        // const gameList = document.getElementById('game-list')
+        let user_id = localStorage.getItem('user_id')
+        console.log(localStorage.getItem('user_id'))
+        console.log(user_id)
+        UsersAdapter.getUserStats(user_id).then(stats => {
+            console.log(stats)
+         
+            stats.forEach(stat => 
+            this.arrayAllStats.push(stat))
+            
+        
+            this.arrayAllStats.slice(1).slice(-5).forEach(stat =>{
+                this.arrayFF.push(stat.fireflies_collected)
+                this.arrayEn.push(stat.enemies_defeated)
+                const li = document.createElement('li')
+                li.innerText = `Fireflies collected: ${stat.fireflies_collected}` + ` Enemies Banished: ${stat.enemies_defeated}`
+                this.gameList.appendChild(li)
+            })
+        
+        const fireFlyH3 = document.createElement('h3')
+        const enemyH3 = document.createElement('h3')
+        let addFF = this.arrayFF.reduce((total, add) => total + add, 0);
+        
+        let addEn = this.arrayEn.reduce((total, add) => total + add, 0);
+        fireFlyH3.innerText = `All Time Fireflies Collected: ${addFF}`
+        enemyH3.innerText = `All Time Enemies Banished: ${addEn}`
+        this.gameData.appendChild(fireFlyH3)
+        this.gameData.appendChild(enemyH3)
+        console.log(stats)
+        console.log(this.arrayAllStats)
+                             
+        
+      
+        })
+    
+      
+    }
+
+
     render(value){    
         const h3 = document.createElement('h3');
         const userFunctions = document.getElementById('user-functions')
