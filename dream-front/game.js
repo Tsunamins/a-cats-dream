3e014c82ffe270d0c638ff3a5662329ea88c4d27
Tsunamins <reillyamr@gmail.com>
2020-01-04 22:50:30 -0500

    reset user_id, remove new Games instance/user_id

--- dream-front/game.js
+++ dream-front/game.js
@@ -2,12 +2,17 @@ import GameScene from './src/game/GameScene.js'
 import TitleScene from './src/game/TitleScene.js'
 
 
+
 let userInput;
 const userInfoForm = document.getElementById('user-info-form');
 userInfoForm.addEventListener('submit', establishUser);
 const userEmailInput = document.getElementById('user-email')
 const userUpdate = document.getElementById('update-form')
 userUpdate.addEventListener('submit', updateUserEmail)
+const logoutLink = document.getElementById('reload');
+logoutLink.addEventListener('click', () => {
+  localStorage.removeItem('user_id');
+})
 
 
 
@@ -16,8 +21,8 @@ function establishUser(event){
   event.preventDefault(); 
   userInput = event.target[0].value
   const user = new Users(userEmailInput.value)
-  const gameUI = new Games();
-  UsersAdapter.createLocalStorage(userInput)
+  //const gameUI = new Games();
+ // UsersAdapter.createLocalStorage(userInput)
  
   const game = new Phaser.Game(config);
   
