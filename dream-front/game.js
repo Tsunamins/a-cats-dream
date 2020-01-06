9d762aac0f9f8ea209d707cb2f8ae3813aba3655
Tsunamins <reillyamr@gmail.com>
2020-01-06 11:24:30 -0500

    organize code, remove extra code

--- dream-front/game.js
+++ dream-front/game.js
@@ -3,13 +3,13 @@ import TitleScene from './src/game/TitleScene.js'
 
 
 
-let userInput;
+
 const userInfoForm = document.getElementById('user-info-form');
-userInfoForm.addEventListener('submit', establishUser);
 const userEmailInput = document.getElementById('user-email')
 const userUpdate = document.getElementById('update-form')
-userUpdate.addEventListener('submit', updateUserEmail)
 const logoutLink = document.getElementById('reload');
+userInfoForm.addEventListener('submit', establishUserWithNewGame);
+userUpdate.addEventListener('submit', updateUserEmail)
 logoutLink.addEventListener('click', () => {
   localStorage.removeItem('user_id');
 })
@@ -17,13 +17,9 @@ logoutLink.addEventListener('click', () => {
 
 
 
-function establishUser(event){
+function establishUserWithNewGame(event){
   event.preventDefault(); 
-  userInput = event.target[0].value
   const user = new Users(userEmailInput.value)
-  //const gameUI = new Games();
- // UsersAdapter.createLocalStorage(userInput)
- 
   const game = new Phaser.Game(config);
   
 }
@@ -37,19 +33,6 @@ function updateUserEmail(event){
   updateDisplay.innerText = `${changeEmail}`
 
 }
-
-
-
-
-
-
-
-
-
-
-
-//let titleScene = new TitleScene();
-//let gameScene = new GameScene();
 
 let config = {
     type: Phaser.AUTO, 
@@ -68,12 +51,4 @@ let config = {
     GameScene,]
    
   };
-
-  
-  
-  //game.scene.add('GameScene', gameScene);
-
-
-
-
  
