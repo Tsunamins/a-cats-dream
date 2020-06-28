a9b5acb7331ff992b8e5a07cd68da1d6d6872ce6
Tsunamins <reillyamr@gmail.com>
2020-06-28 12:41:27 -0400

    add name field to users

--- dream-back/db/schema.rb
+++ dream-back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_28_023739) do
+ActiveRecord::Schema.define(version: 2020_06_28_160308) do
 
   create_table "games", force: :cascade do |t|
     t.integer "user_id"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_06_28_023739) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "name"
   end
 
 end
