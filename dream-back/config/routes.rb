c30dffd1525e509ac28825c35bd20b4722673c99
Tsunamins <reillyamr@gmail.com>
2020-06-28 12:41:11 -0400

    add session logins/signups to routes

--- dream-back/config/routes.rb
+++ dream-back/config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   resources :games
   resources :stats
 
+  post "/login", to: "sessions#create"
+  post "/signup", to: "users#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/get_current_user", to: "sessions#get_current_user"
+
   resources :users do
     resources :games
   end
