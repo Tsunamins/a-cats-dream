04a9bef6caacd14fdbbada3315b5a1731f119095
Tsunamins <reillyamr@gmail.com>
2020-06-27 22:40:59 -0400

    add password field to users

--- dream-back/db/schema.rb
+++ dream-back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_05_012238) do
+ActiveRecord::Schema.define(version: 2020_06_28_023739) do
 
   create_table "games", force: :cascade do |t|
     t.integer "user_id"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_01_05_012238) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
