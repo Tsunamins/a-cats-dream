1b35e85f153d5e44d261a228d62c2c6eeab4ab4a
Alison Reilly <reillyamr@gmail.com>
2020-09-16 14:57:26 -0400

    use set_user and clean up code

--- dream-back/app/controllers/users_controller.rb
+++ dream-back/app/controllers/users_controller.rb
@@ -1,60 +1,50 @@
 class UsersController < ApplicationController
-    skip_before_action :user_not_logged_in, only: [:new, :create]
+    before_action :set_user, only: [:show, :update, :destroy]
     def show
-        @user = User.find_by(id: params[:id])
-        
-        # @user = User.find(params[:id])
-        # render json: @user, status: 200
-        
         user_json = UserSerializer.new(@user).serialized_json
         render json: user_json
     end
 
     def index
-     
         @users = User.all
-        # @users = User.all
-        # render json: @users, status: 200 
         users_json = UserSerializer.new(@users).serialized_json
         render json: users_json
     end 
 
     def create 
-    @user = User.new(user_params)
-    # binding.pry
-     if @user.save
-      session[:user_id] = @user.id
-      render json: UserSerializer.new(@user), status: :created
-      #render json: @user, status: :created
-    else
-      resp = {
-        error: @user.errors.full_messages.to_sentence
-      }
-      render json: resp, status: :unprocessable_entity
-    end
-     
-    #current setup:
-    # @user = User.find_or_create_by(email: user_params[:email])
-
-    # render json: @user, status: 200
-      
-    end
+        @user = User.new(user_params)
+        # binding.pry
+        if @user.save
+            session[:user_id] = @user.id
+            render json: UserSerializer.new(@user), status: :created
+        else
+            resp = {
+                error: @user.errors.full_messages.to_sentence
+            }
+            render json: resp, status: :unprocessable_entity
+        end
+     end
 
     def update
-       
-        @user = User.find(params[:id])
-        @user.update(user_params)
-        render json: @user, status: 200
-         
+        if @user.update(user_params)
+            render json: @user
+          else
+            render json: @user.errors, status: :unprocessable_entity
+          end         
     end
 
     def destroy
-        @user = User.find(params[:id])
         @user.delete
         render json: {userId: @user.id} 
     end
 
     private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_user
+            @user = User.find(params[:id])
+        end
+
+        # Only allow a trusted parameter "white list" through.
         def user_params
             params.require(:user).permit(:email, :name, :password)
         end 
