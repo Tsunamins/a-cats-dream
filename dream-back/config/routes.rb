b916796ada06d9290f06d73bb185c77d369fb240
Alison Reilly <reillyamr@gmail.com>
2021-01-18 17:51:35 -0500

    remove example

--- dream-back/config/routes.rb
+++ dream-back/config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
   
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'private/hello'
-  get 'public/hello'
-  resources :chirps
 
  
   resources :games
   resources :users
 
-  # resources :users do
-  #   resources :games
-  # end
+  resources :users do
+    resources :games
+  end
 
  
 
